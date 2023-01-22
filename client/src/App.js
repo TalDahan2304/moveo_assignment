0a91bb75eda0fe5566ceda5b1d895f256b5faa24
TalDahan2304 <tald234097@gmail.com>
2023-01-22 19:59:53 +0200

    routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,15 @@
 import './App.css';
+import { BrowserRouter ,Routes, Route } from "react-router-dom";
+import PageCodeList from './pages/Lobby';
 
 function App() {
   return (
     <div className="App">
-      
+      <BrowserRouter>
+        <Routes path="/">
+         <Route index element={<PageCodeList/>}></Route>
+        </Routes>  
+      </BrowserRouter>
     </div>
   );
 }
